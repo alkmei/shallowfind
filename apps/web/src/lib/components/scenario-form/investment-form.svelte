126dd2eb9a20c5511ee02af72ec565f4de21f99f
Alvin Mei <alvinkmei@gmail.com>
2025-07-17 20:49:14 -0400

    Continued work on scenario form

--- apps/web/src/lib/components/scenario-form/investment-form.svelte
+++ apps/web/src/lib/components/scenario-form/investment-form.svelte
@@ -2,14 +2,10 @@
   import type { scenariosCreateBody } from '$lib/api/scenarios/scenarios.zod';
   import type { SuperForm } from 'sveltekit-superforms';
   import z from 'zod';
-  import Button from '../ui/button/button.svelte';
   import type { InvestmentType } from '$lib/api/shallowfind.schemas';
-  import { Plus } from '@lucide/svelte';
-  import * as Dialog from '$lib/components/ui/dialog';
-  import { Label } from '../ui/label';
-  import { Input } from '../ui/input';
-  import { Textarea } from '../ui/textarea';
   import InvestmentTypeModal from './investment-type-modal.svelte';
+  import * as Card from '$lib/components/ui/card';
+  import InvestmentModal from './investment-modal.svelte';
 
   type ScenariosCreateBody = z.infer<typeof scenariosCreateBody>;
 
@@ -18,15 +14,26 @@
   let investmentTypes: InvestmentType[] = $state([]);
 </script>
 
-<div class="flex flex-col gap-3">
+<div class="mb-4 flex flex-col gap-8">
   <div class="flex justify-between">
     <h2 class="grow text-xl font-bold">Investment Types</h2>
     <InvestmentTypeModal {investmentTypes} />
   </div>
   {#if investmentTypes.length > 0}
-    <ul class="rounded border p-2">
+    <ul class="flex flex-col gap-2 rounded border p-2">
       {#each investmentTypes as type (type.name)}
-        <li>{type.name} - {type.description}</li>
+        <Card.Root>
+          <Card.Header>
+            <Card.Title>{type.name}</Card.Title>
+            <Card.Description>{type.description}</Card.Description>
+          </Card.Header>
+          <Card.Content>
+            <p>Return: {type.returnDistribution.mean} {type.returnAmtOrPct}</p>
+            <p>Expense Ratio: {type.expenseRatio}%</p>
+            <p>Income: {type.incomeDistribution.mean} {type.incomeAmtOrPct}</p>
+            <p>Taxable: {type.taxability ? 'Yes' : 'No'}</p>
+          </Card.Content>
+        </Card.Root>
       {/each}
     </ul>
   {:else}
@@ -34,4 +41,11 @@
       <p class="text-gray-400">No investment types available. Please add some.</p>
     </div>
   {/if}
+</div>
+
+<div class="flex flex-col gap-3">
+  <div class="flex justify-between">
+    <h2 class="grow text-xl font-bold">Investments</h2>
+    <InvestmentModal {form} {investmentTypes} />
+  </div>
 </div>
