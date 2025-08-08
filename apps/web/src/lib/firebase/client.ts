88f4c398a647ee47b2b68761e9655514322bb14a
Alvin Mei <alvinkmei@gmail.com>
2025-08-08 00:40:39 -0400

    Added firebase config

--- apps/web/src/lib/firebase/client.ts
+++ apps/web/src/lib/firebase/client.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: 'AIzaSyArNUPcwUwbPjk6Jxn5YQEZo1m8ilEBY30',
+  authDomain: 'shallowfind-df121.firebaseapp.com',
+  projectId: 'shallowfind-df121',
+  storageBucket: 'shallowfind-df121.firebasestorage.app',
+  messagingSenderId: '454327889819',
+  appId: '1:454327889819:web:0b96349a1d64af5b903b3e'
 };
 
 const app = initializeApp(firebaseConfig);
