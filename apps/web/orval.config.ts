4bb80d129834d51700a958971ab84fbedbecc3a2
Alvin Mei <alvinkmei@gmail.com>
2025-08-08 20:36:50 -0400

    added generated orval

---
+++ apps/web/orval.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+  scenario: {
+    input: '../../services/scenario-manager/swagger.json',
+    output: {
+      mode: 'tags-split',
+      target: './src/lib/api/scenario-management',
+      client: 'svelte-query',
+      override: {
+        query: {
+          options: {
+            useQuery: true,
+            staleTime: 1000 * 60 * 5 // 5 minutes
+          }
+        }
+      }
+    }
+  },
+  scenarioZod: {
+    input: '../../services/scenario-manager/swagger.json',
+    output: {
+      mode: 'tags-split',
+      target: './src/lib/api/scenario-management',
+      client: 'zod',
+      fileExtension: '.zod.ts'
+    }
+  }
+});
