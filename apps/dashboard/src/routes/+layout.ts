c11abf57c576bb8dc30fc726863f99b6ca064b86
Alvin Mei <alvinkmei@gmail.com>
2025-08-28 00:27:38 -0400

    Removed github pages, added new sveltekit

--- apps/dashboard/src/routes/+layout.ts
+++
@@ -1,3 +0,0 @@
-export const prerender = false;
-export const ssr = false; // SPA mode
-export const csr = true;
--- apps/dashboard/svelte.config.js
+++ apps/dashboard/svelte.config.js
@@ -1,7 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,16 +7,9 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		}),
-		appDir: 'app',
-		paths: {
-			base: dev ? '' : '/shallowfind/dashboard'
+		adapter: adapter(),
+		alias: {
+			'@/*': './path/to/lib/*'
 		}
 	}
 };
